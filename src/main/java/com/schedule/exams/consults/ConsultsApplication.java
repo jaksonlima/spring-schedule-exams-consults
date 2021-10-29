8404a8f0754f501447aa2b06e4f9c5d62db6d1e7
jaksonlima <jaksonsneider@gmail.com>
2021-10-28 21:30:19 -0300

    first commit

---
+++ src/main/java/com/schedule/exams/consults/ConsultsApplication.java
@@ -0,0 +1,13 @@
+package com.schedule.exams.consults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedule/exams/consults/ConsultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule.exams.consults;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
